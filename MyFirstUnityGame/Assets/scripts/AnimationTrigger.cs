6c29921bd8775965bc94d4a87b8d3051473c36db
Evert <37932@ma-web.nl>
2024-03-21 12:07:07 +0100

    E

--- MyFirstUnityGame/Assets/scripts/AnimationTrigger.cs
+++ MyFirstUnityGame/Assets/scripts/AnimationTrigger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnimationTrigger : MonoBehaviour
@@ -8,28 +9,20 @@ public class AnimationTrigger : MonoBehaviour
 
     void Start()
     {
-        //Get the Animator attached to the GameObject you are intending to animate.
         m_Animator = gameObject.GetComponent<Animator>();
     }
 
     void Update()
     {
-        //Press the up arrow button to reset the trigger and set another one
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            //Reset the "Crouch" trigger
             m_Animator.ResetTrigger("Trigger");
-
-            //Send the message to the Animator to activate the trigger parameter named "Jump"
             m_Animator.SetTrigger("Trigger2");
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            //Reset the "Jump" trigger
             m_Animator.ResetTrigger("Trigger2");
-
-            //Send the message to the Animator to activate the trigger parameter named "Crouch"
             m_Animator.SetTrigger("Trigger");
         }
     }
