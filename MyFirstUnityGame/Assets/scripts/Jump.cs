ee54e95eabea18bdb9db08eb92b67d454f00725a
Evert <37932@ma-web.nl>
2024-03-21 13:02:08 +0100

    Klaar3

--- MyFirstUnityGame/Assets/scripts/Jump.cs
+++ MyFirstUnityGame/Assets/scripts/Jump.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody rb;
+    public float force = 50f;
+    public bool onFloor = false;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            onFloor = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            onFloor = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && onFloor == true)
+        {
+            rb.AddForce(Vector3.up * force);
+        }
     }
 }
